93523d1737f572a746c2db381a6ec6d9153a8cfc
Gleejay <768379073@qq.com>
2018-12-18 20:25:42 +0800

    zzx maven项目 Persion

---
+++ zzxmavendemo/src/main/java/com/gleejay/cn/Persion.java
@@ -0,0 +1,20 @@
+package com.gleejay.cn;
+
+import javax.naming.Name;
+
+/**
+ * @author gleejay
+ * @date 2018-12-18
+ */
+public class Persion {
+    private static final int age1 = 3;
+    /** age1 */
+    private int age;
+
+    public Persion(int age) {
+        this.age = age;
+    }
+
+    public Persion() {
+    }
+}
