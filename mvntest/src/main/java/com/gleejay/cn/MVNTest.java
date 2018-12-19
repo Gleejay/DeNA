0309a956f103c4f094115bd1206c44788addb532
Gleejay <768379073@qq.com>
2018-12-19 10:45:18 +0800

    maven项目 20181219 10:45

---
+++ mvntest/src/main/java/com/gleejay/cn/MVNTest.java
@@ -0,0 +1,15 @@
+package com.gleejay.cn;
+
+/**
+ * @author gleejay
+ * @date 2018-12-18
+ */
+public class MVNTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("maven Test Project Module");
+
+        System.out.println("AAAAAAAAAAAAAAAAA");
+    }
+}
